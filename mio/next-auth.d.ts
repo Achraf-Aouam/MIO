5af5b41f0b36fbaf1a03268f09bb785d7efe8f99
Achraf-Aouam <a.aouam@aui.ma>
2024-05-08 13:41:02 +0000

    auth fixed-ish

--- mio/next-auth.d.ts
+++ mio/next-auth.d.ts
@@ -1,13 +1,13 @@
-// // import { UserRole } from "@prisma/client";
-// import NextAuth, { type DefaultSession } from "next-auth";
-// import { UserRole } from "@prisma/client";
+import { UserRole } from "@prisma/client";
+import NextAuth, { type DefaultSession } from "next-auth";
+import { UserRole } from "@prisma/client";
 
-// export type ExtendedUser = DefaultSession["user"] & {
-//   role: UserRole;
-// };
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+};
 
-// declare module "next-auth" {
-//   interface Session {
-//     user: ExtendedUser;
-//   }
-// }
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
